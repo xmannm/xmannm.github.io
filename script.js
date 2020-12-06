8a554157460363d29694d7ec5619aff527e51ab8
ocasusMaximus <za.jan@seznam.cz>
2020-12-06 17:36:30 +0100

    Add sticky navbar fontsize change

---
+++ script.js
@@ -0,0 +1,55 @@
+window.onscroll = function () { myFunction() };
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+//	var dropdown = document.getElementsByClassName("dropdown");
+
+function myFunction() {
+    var items = navbar.getElementsByTagName("a")
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky");
+        for (var i = 0; i < items.length; i++) {
+            items[i].style.fontSize = "20px";
+        }
+    } else {
+        navbar.classList.remove("sticky");
+        for (var i = 0; i < items.length; i++) {
+            items[i].style.fontSize = "30px";
+        }
+    }
+   
+
+       
+
+        
+       
+
+    
+}
+/*text slide show */
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
